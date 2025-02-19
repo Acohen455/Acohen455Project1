1995699ad690c8249b42eb943e6f54b72faec1fa
Acohen455 <aaroncohen455@gmail.com>
2025-02-19 11:04:57 -0700

    Updates

--- Project1Backend/src/main/java/com/revature/Models/Reimbursement.java
+++ Project1Backend/src/main/java/com/revature/Models/Reimbursement.java
@@ -30,6 +30,72 @@ public class Reimbursement {
     private int userId;
 
 
+    //constructors
+    public Reimbursement() {
+    }
+
+    public Reimbursement(int reimbursementId, String description, double amount, boolean pending, int userId) {
+        this.reimbursementId = reimbursementId;
+        this.description = description;
+        this.amount = amount;
+        this.pending = pending;
+        this.userId = userId;
+    }
 
 
+    //setters and getters
+    public int getReimbursementId() {
+        return reimbursementId;
+    }
+
+    public void setReimbursementId(int reimbursementId) {
+        this.reimbursementId = reimbursementId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+
+    public boolean isPending() {
+        return pending;
+    }
+
+    public void setPending(boolean pending) {
+        this.pending = pending;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public void setUserId(int userId) {
+        this.userId = userId;
+    }
+
+    //toString
+    //TODO: Customize this later
+
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Reimbursement{" +
+                "reimbursementId=" + reimbursementId +
+                ", description='" + description + '\'' +
+                ", amount=" + amount +
+                ", pending=" + pending +
+                ", userId=" + userId +
+                '}';
+    }
 }
